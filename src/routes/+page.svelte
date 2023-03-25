7266111e7010815ce0e7e90ac5acfe3ecaf62f4d
Djura Smits <djura.smits@gmail.com>
2023-03-25 12:59:38 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="hero min-h-screen" style="background-image: url(/save-the-date.png);">
+    <div class="hero-overlay bg-transparent/0"></div>
+</div>
+
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    </div>
+    <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+        </ul>
+    </div>
+</div>
