c6e64e1b59bb845588b2ef0af2526d627a800bd7
Djura Smits <djura.smits@gmail.com>
2023-03-25 15:56:39 +0100

    Added pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,20 @@
 <script>
-    import Description from './Description.svelte'
+    import Description from './Description.svelte';
+    import Levels from './Levels.svelte';
+    import { base } from "$app/paths";
+    import Concepts from "./Concepts.svelte";
 </script>
 
-<div class="hero min-h-screen" style="background-image: url(/save-the-date.png);">
+<div class="hero min-h-screen" style="background-image: url({base}/save-the-date.png);">
     <div class="hero-overlay bg-transparent/0"></div>
 </div>
 
-<div class="navbar bg-base-100">
-    <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-    </div>
-    <div class="flex-none">
-        <ul class="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
-        </ul>
-    </div>
-</div>
 <div>
     <Description/>
+</div>
+<div>
+    <Levels/>
+</div>
+<div>
+    <Concepts/>
 </div>
