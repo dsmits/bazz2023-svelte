606ff085c4ffc16031cf762d82b9dc448cac7b35
Djura Smits <djura.smits@gmail.com>
2023-03-25 15:51:17 +0100

    Added npm install to workflow

---
+++ src/routes/Concepts.svelte
@@ -0,0 +1,9 @@
+
+CONCEPTS @ BAZZ
+
+BAZZ Lab is focused on a crossover between two different dance styles. One Jazz teacher, and one teacher of another dance style will be guiding these sessions together. Let's explore existing links between Jazz and its contemporary descendant dance styles, let's celebrate their similarities and differences! (Included in all class passes)
+
+JAZZ Hub session is totally focused on Jazz! In this session we will bring all our badass Jazz teachers together in the same room to share their diverse perspectives on many motives that create a Jazz landscape. With a goal to help you find your own truth within a Jazz universe, we will tackle topics like groove, improvisation, authenticity, culture, jamming etc. If you're wondering how this will look like, probably more like a party rather than a class ;)
+(NOT included in any pass, you can sign up separately in registration form)
+
+BAZZ Buzz is focused on YOU! This time will be dedicated to your own practice, where together with your BAZZ friends, you can go through whatever you learned throughout the weekend, or you can just jam ! In each BB session there will be a DJ & Teachers available for you to ask any questions, personal feedback or tips! You're not obliged to ask them anything, you can also just come and hang, listen to music & grab a drink :) (Included in all class passes)
---
+++ src/routes/Levels.svelte
@@ -0,0 +1,15 @@
+LEVELS:
+<p>
+Note for enthusiastic & passionate learners:
+KICK ASS level & KICK ASS PRO are oriented towards enthusiastic dancers and passionate learners that want a total immersion of various Social and Club dance styles throughout this weekend! These levels are available only within a FULL PASS package
+</p>
+<p>
+KICK ASS is recommended for INT/INT+. You're comfortable with weight shifts and moving through space. You're familiar with most of the basic vocabulary, but some pieces are missing. You're still searching to expand your knowledge on basics, transitions and body awareness. You need more guidance around improvisation and generally throughout the class, but you're ready to push yourself outside of your comfort zone and finish the weekend like a Bad Ass.
+</p>
+<p>
+BAZZ PRO is recommended for ADV/ADV+ dancers with good body awareness, body control, and fluent improvisation skills, who want to challenge their current limits. You easily follow combinations, you're enthusiastic about exploring intricate rhythms, techniques and movement in greater depth, while adding your own voice to it. You're comfortable with a faster pace of learning, you're fluent in improvisation but want to level it up like pro!
+</p>
+<p>
+For Chillers who just want to have fun:
+CHILLAX Pass is for everyone who wants a taste of BAZZ Vibes, but still wants to have enough time to explore Amsterdam and relax during the day. This group will have an easy going schedule with less number of classes than a full pass, scheduled in the afternoon. It will be an OPEN level, which means approachable to a wide range of dance experience.
+</p>
