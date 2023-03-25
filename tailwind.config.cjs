8b01fc553247fad1da2572452184f77c992bd4a4
Djura Smits <djura.smits@gmail.com>
2023-03-25 15:55:41 +0100

    Added more config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,23 @@ const config = {
   theme: {
     extend: {},
   },
-
+  daisyui: {
+    themes: [
+      {
+        bazz: {
+          "primary": "#ace285",
+          "secondary": "#5ee087",
+          "accent": "#20bf62",
+          "neutral": "#272537",
+          "base-100": "#8f794b",
+          "info": "#89ADEC",
+          "success": "#78DEBC",
+          "warning": "#91650D",
+          "error": "#E6283F",
+        },
+      },
+    ],
+  },
   plugins: [require('daisyui')],
 };
 
