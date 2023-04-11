1a5b02791e2d083e353574d86720d0cde20ef8ba
Djura Smits <djura.smits@gmail.com>
2023-04-11 15:54:03 +0200

    hopefully fixed youtube embeds

--- src/routes/Previous.svelte
+++ src/routes/Previous.svelte
@@ -1,12 +1,12 @@
 <article class="prose prose-xl text-center">
     <h1>Previous Edition</h1>
-   <p> <iframe width="560" height="315" src="https://www.youtube.com/embed/ElumjRqXX-M" title="YouTube video player"
+   <p class="flex justify-center"> <iframe class="text-center justify-center justify-items-center" src="https://www.youtube.com/embed/ElumjRqXX-M" title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen></iframe>
    </p>
-    <p>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/pVPRf5hBv90" title="YouTube video player"
+    <p class="flex justify-center">
+    <iframe src="https://www.youtube.com/embed/pVPRf5hBv90" title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen></iframe>
