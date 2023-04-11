5eed9a0541f079b190d5ccca4261d06cc2c9c04b
Djura Smits <djura.smits@gmail.com>
2023-04-11 16:09:19 +0200

    Bazz vibes

--- src/routes/Previous.svelte
+++ src/routes/Previous.svelte
@@ -1,5 +1,5 @@
 <article class="prose prose-xl text-center">
-    <h1>Previous Edition</h1>
+    <h1>Bazz Vibes</h1>
    <p class="flex justify-center"> <iframe class="text-center justify-center justify-items-center" src="https://www.youtube.com/embed/ElumjRqXX-M" title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
