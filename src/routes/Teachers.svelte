36928ea7f5a3a773379499ec2e528bd02b853fe3
Djura Smits <djura.smits@gmail.com>
2023-07-15 13:03:03 +0200

    Scrollable caroussel

--- src/routes/Teachers.svelte
+++ src/routes/Teachers.svelte
@@ -12,6 +12,20 @@
                 "She has been teaching all over the Europe as well as Houston, Seoul, Bangkok." +
                 "She endlessly continues to fuel her inspiration with various other dance styles. Her huge wish is to connect Jazz and Street dance communities and bring this dance to street jams and clubs!",
             "image": "helena.png"
+        },
+        {
+            "name": "Helena Kanini Kiiru",
+            "description": "Helena Kanini Kiiru is a young dancer and teacher from Zagreb, Croatia, currently based in Amsterdam. Her enthusiasm for music and dancing has been evident since her childhood when she showed great talent for playing piano at the age of 7. Having Kenyan roots in her family, Helena has always been exposed to African music, which stimulated her sensibility for the rhythm. Very soon she started developing interest towards dances like Hip Hop and Dancehall. After 8 years of exploring various Street dance styles, she saw African American Jazz dancing and immediately felt that it speaks to her on a very deep level. Helena feels that Jazz created an opportunity for her to meet and express all the deep parts of herself that made her expand and grow. Currently based in Amsterdam, she continues to teach locally, internationally and online.\n" +
+                "She has been teaching all over the Europe as well as Houston, Seoul, Bangkok." +
+                "She endlessly continues to fuel her inspiration with various other dance styles. Her huge wish is to connect Jazz and Street dance communities and bring this dance to street jams and clubs!",
+            "image": "helena.png"
+        },
+        {
+            "name": "Helena Kanini Kiiru",
+            "description": "Helena Kanini Kiiru is a young dancer and teacher from Zagreb, Croatia, currently based in Amsterdam. Her enthusiasm for music and dancing has been evident since her childhood when she showed great talent for playing piano at the age of 7. Having Kenyan roots in her family, Helena has always been exposed to African music, which stimulated her sensibility for the rhythm. Very soon she started developing interest towards dances like Hip Hop and Dancehall. After 8 years of exploring various Street dance styles, she saw African American Jazz dancing and immediately felt that it speaks to her on a very deep level. Helena feels that Jazz created an opportunity for her to meet and express all the deep parts of herself that made her expand and grow. Currently based in Amsterdam, she continues to teach locally, internationally and online.\n" +
+                "She has been teaching all over the Europe as well as Houston, Seoul, Bangkok." +
+                "She endlessly continues to fuel her inspiration with various other dance styles. Her huge wish is to connect Jazz and Street dance communities and bring this dance to street jams and clubs!",
+            "image": "helena.png"
         }
 
     ]
@@ -20,7 +34,7 @@
     <h1 class="text-center">Teachers</h1>
 
 </article>
-<div class="carousel rounded-box">
+<div class="carousel w-full rounded-box">
 {#each teachers as teacher, i}
 
     <div id="slide-{i}" class="carousel-item">
@@ -32,7 +46,6 @@
 
             </div>
         </div>
-
         <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             {#if i <= 0}
                 <a href="#slide-{teachers.length-1}" class="btn btn-circle">❮</a>
