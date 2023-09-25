69285b47eb00f1be026b177b8e851f965b7bc601
Djura Smits <djura.smits@gmail.com>
2023-09-25 22:20:09 +0200

    CHanged build scfript

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,13 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: preprocess(),
     kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: null
-        }),
-
-    },
-
-
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
+    }
 };
-
-export default config;
