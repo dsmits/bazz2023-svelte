9aa84facb5b0382a25ecd2e189369c6274eef81b
Djura Smits <djura.smits@gmail.com>
2023-09-25 22:23:59 +0200

    Revert "CHanged build scfript"
    This reverts commit 69285b47eb00f1be026b177b8e851f965b7bc601.

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
-import adapter from '@sveltejs/adapter-static';
-
-const dev = process.argv.includes('dev');
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    preprocess: preprocess(),
     kit: {
-        adapter: adapter(),
-        paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
-    }
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
+
+    },
+
+
 };
+
+export default config;
