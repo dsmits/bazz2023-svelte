f9425fc09bda4e89e69e629ff899f55fe7b8c450
Djura Smits <djura.smits@gmail.com>
2023-10-17 19:15:11 +0200

    Created carousel component

---
+++ src/routes/Carousel.svelte
@@ -0,0 +1,18 @@
+<script>
+    import CarouselItem from './CarouselItem.svelte';
+    export let itemWidth = "w-96"
+    export let items;
+</script>
+
+<div class="carousel w-full rounded-box">
+    {#each items as item, i}
+        <CarouselItem id="slide{i}" name="{item.name}" image="{item.image}" description="{item.description}"
+                      width="{itemWidth}"></CarouselItem>
+    {/each}
+
+</div>
+<div class="flex justify-center w-full py-2 gap-2">
+    {#each items as item, i}
+        <a href="#slide-{i}" class="btn btn-xs">{i + 1}</a>
+    {/each}
+</div>
---
+++ src/routes/CarouselItem.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let id;
+    export let image;
+    export let name;
+    export let description;
+    export let width;
+</script>
+
+<div id="{id}" class="carousel-item">
+    <div class="card card-compact {width} bg-base-100 shadow-xl">
+        <figure><img src="{image}" alt="{name}"/></figure>
+        <div class="card-body">
+            <h2 class="card-title">{name}</h2>
+            <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-200">
+
+                <div class="collapse-title text-xl font-medium">
+                    More info
+                </div>
+                <div class="collapse-content">
+                    <p>{description}</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
